c780e91d9fa152f2b61b6a43f504f60fde08ff3d
juan <juan2ramos@gmail.com>
2018-08-24 17:19:22 -0500

    imagem

--- page.php
+++ page.php
@@ -2,11 +2,11 @@
 <?php echo do_shortcode("[rev_slider alias='principal']"); ?>
     <main class="container row">
         <div class="space col-16"></div>
-        <section class="Categories col-4">
+        <section class="Categories col-16 col-l-4">
             <h2>Categorias</h2>
             <?php get_sidebar() ?>
         </section>
-        <section class=" col-12 row justify-between ">
+        <section class=" col-16  col-l-12 row justify-between ">
             <div class="Category-title col-16 justify-center">
                 <h1> <?php the_title(); ?></h1>
             </div>
